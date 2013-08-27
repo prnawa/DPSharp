d5fe8575a6e22aa9626da1b695391be80f392d33
Ruwan Nawarathne <prnawa@gmail.com>
2013-08-27 11:15:04 +0530

    updating builder test

--- DesignPatternTests/BuilderPatternTests.cs
+++ DesignPatternTests/BuilderPatternTests.cs
@@ -10,19 +10,20 @@ namespace DesignPatternTests
         public void Test()
         {
             var director = new Director();
-            var builder1 = new Builder1();
-            var builder2 = new Builder2();
+            Builder builder = null;
 
-            director.Construct(builder1);
-            director.Construct(builder2);
+            builder = new Builder1();
+            director.Construct(builder);
+            var result1 = builder.GetResult();
 
-            var result1 = builder1.GetResult();
-            var result2 = builder2.GetResult();
+            builder = new Builder2();
+            director.Construct(builder);
+            var result2 = builder.GetResult();
 
             Assert.AreEqual("Part A", result1.PartOne);
-            Assert.AreEqual("Part X", result2.PartOne);
-
             Assert.AreEqual("Part B", result1.PartTwo);
+
+            Assert.AreEqual("Part X", result2.PartOne);
             Assert.AreEqual("Part Y", result2.PartTwo);
         }
     }
